9db0e905c5c89270bf38c44c593af1e15c32a123
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-01-23 19:52:30 +0530

    githubworkshop

--- Kotlin/1_variables.kt
+++ Kotlin/1_variables.kt
@@ -1,10 +1,10 @@
 //	Declare variables of the following types and assign appropriate values: Int,Double,Boolean,String
 
 fun main() {
-    val myInt:  = 42 
-    val myDouble: Double 
-    val myBoolean: 
-    val myString: String = Hello, Kotlin!
+    val myInt:Int  = 42 
+    val myDouble: Double = 3.412
+    val myBoolean: Boolean = true
+    val myString: String = "Hello, Kotlin!"
 }
 
 //correct the given code
