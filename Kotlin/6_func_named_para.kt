6c9fb152066597ec6f39514c4c8e7d3f1439c66b
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-01-27 19:40:43 +0530

    androidWorkshop6.kt

--- Kotlin/6_func_named_para.kt
+++ Kotlin/6_func_named_para.kt
@@ -2,10 +2,16 @@
 
 // in your function, declare name first then age
 
+fun displayPerson(name: String, age: Int){
+    println("I am $name and I am $age (which means I have been around the sun $age times, cool isn't it!)")
+}
+
 fun main() {
 
     displayPerson(age = 25, name = "Charlie")
+    displayPerson(age= 19, name = "yahaswini")
     
 }
 
 // when you specifically use the name of the parameter, you can use it in any order, you cannot do this in c++ or java
+//ngl this aspect of parameter is so cool and interesting
