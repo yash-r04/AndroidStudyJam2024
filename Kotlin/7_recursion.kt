793cd609f5ebf5a22a89a33c3aea7cc6c195287f
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-01-27 19:58:14 +0530

    androidWorkshop 7_recursion.kt

--- Kotlin/7_recursion.kt
+++ Kotlin/7_recursion.kt
@@ -1,10 +1,24 @@
 // implement a recursive function called factorial that calculates the factorial of a given non-negative integer.
 
 // implement your function here
+fun factorial(number: Int): Int{
+    if (number==0 || number==1){
+        return 1
+    }
+    else
+    {
+      return number * factorial(number-1)
+      }
+      
+    }
 
 fun main() {
     val result = factorial(5)
+    val res = factorial(1)
+    val re = factorial(0)
     println(result)
+    println(res)
+    println(re)
 }
 
 // function that calls itself is called recursive function
