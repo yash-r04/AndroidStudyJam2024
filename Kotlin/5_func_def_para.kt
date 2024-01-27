3fd188b2dd7e711f1201916418646b5860eb3c0d
yash-r04 <150778551+yash-r04@users.noreply.github.com>
2024-01-27 19:34:24 +0530

    androidWorkshop.kt

--- Kotlin/5_func_def_para.kt
+++ Kotlin/5_func_def_para.kt
@@ -1,7 +1,9 @@
 // Create a function called printInfo that takes a name and an age as parameters. Age should have a default value of 25. Print the information.
 
 // you function here
-
+fun printInfo(name: String, age: Int=25){
+    println("hello, I am $name and I am $age years old :)")
+}
 
 fun main() {
     printInfo("Alice")
